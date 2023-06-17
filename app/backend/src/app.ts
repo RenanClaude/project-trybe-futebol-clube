2daba267c3b0b556c98fa92bb6e15347a7650937
Renan Claude <renanclaude.rcg@gmail.com>
2023-06-17 01:47:01 -0300

    Requirement 13 - done

--- app/backend/src/app.ts
+++ app/backend/src/app.ts
@@ -1,19 +1,8 @@
 import * as express from 'express';
-// import { Request, Response, NextFunction } from 'express';
-// import TeamsController from './controllers/teamsController';
-// import LoginController from './controllers/loginController';
-// import Encryptor from './middlewares/encryptor';
-// import LoginService from './sevices/loginService';
-// import LoginMiddleware from './middlewares/loginMiddleware';
-// import Token from './middlewares/token';
 import router from './routes';
 
 class App {
   public app: express.Express;
-  // public teamsController = new TeamsController();
-  // public encryptor = new Encryptor();
-  // public loginService = new LoginService();
-  // public loginController = new LoginController(this.loginService, this.encryptor);
 
   constructor() {
     this.app = express();
@@ -39,21 +28,6 @@ class App {
 
     this.app.use(express.json());
     this.app.use(accessControl);
-
-    // // Endpoint - Requisito 3
-    // this.app.get('/teams', (req, res) => this.teamsController.getAllTeamsController(req, res));
-
-    // // Endpoint - Requisito 5
-    // this.app.get('/teams/:id', (req: Request, res: Response) => this.teamsController
-    //   .getTeamByIdController(req, res));
-
-    // // Endpoint - Requisito 8
-    // this.app.post(
-    //   '/login',
-    //   (req: Request, res: Response, next: NextFunction) => LoginMiddleware
-    //     .loginValidations(req, res, next),
-    //   (req, res) => this.loginController.loginController(req, res),
-    // );
   }
 
   public start(PORT: string | number): void {
